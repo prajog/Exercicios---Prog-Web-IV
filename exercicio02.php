dc9c36fbdd2eae00399791a11c01a5be4c8a90e8
prajog <gabrielapberetta@gmail.com>
2024-09-26 11:16:33 -0300

    Criando respositório com os dois primeiros exercícios

---
+++ exercicio02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1><b>URLs Absolutas</b></h1> </br>";
+    echo "<a href='https://www.w3.org/'>W3C</a> </br></br>";
+
+    echo "<a href='https://www.google.com/'>Google</a> </br></br>";
+
+    echo "<h1><b>URLs Relativas</b></h1> </br>";
+    echo "<a href='https://www.w3schools.com/html/html_images.asp'>HTML Images</a> </br></br>";
+
+    echo "<a href='https://www.w3schools.com/css/'>CSS Tutorial</a> </br>";
+
+
+    ?>
+</body>
+</html>
